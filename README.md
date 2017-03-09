babe90848fd8371570f87a377b3b87815b35e9d0
lubezhang <lubezhang@gmail.com>
2017-03-09 14:07:35 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Spder House
+
+## Install
+
+## Build
+The code of the spder-house is located in the folder `./lib`. To build 
+spder-house:
+
+- Install dependencies:
+
+  ```
+  npm install
+  ```
+
+- Build JSON Editor:
+
+  ```
+  npm start
+  ```
---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict"
+
+let { HouseServer } = require("./lib/HouseServer");
+let AJK = require("./lib/house/AJK").AJK;
+let Fang = require("./lib/house/Fang").Fang;
+
+let AJKServer = new HouseServer(new AJK());
+AJKServer.start();
+
+let FangServer = new HouseServer(new Fang());
+FangServer.start();
