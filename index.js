f58c879b8957fa249e323f25f366635b627dd58a
lubezhang <lubezhang@gmail.com>
2017-03-14 11:36:54 +0800

    日志信息写入到日志文件中，支持不同房屋数据源分别记录日志

--- index.js
+++ index.js
@@ -4,11 +4,3 @@ let { HouseServer } = require("./lib/HouseServer");
 
 let houseServer = new HouseServer();
 houseServer.start();
-// let AJK = require("./lib/house/AJK").AJK;
-// let Fang = require("./lib/house/Fang").Fang;
-
-// let AJKServer = new HouseServer(new AJK());
-// AJKServer.start();
-
-// let FangServer = new HouseServer(new Fang());
-// FangServer.start();
